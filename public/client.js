8c5c8a325b18ba4a203f44eb4d9f0d19dabc6868
Neota Moe <neota.moe@gmail.com>
2017-04-28 15:34:56 -0500

    delete pet added

--- public/client.js
+++ public/client.js
@@ -3,6 +3,120 @@ console.log('scripts sourced');
 $(document).ready(onReady);
 
 function onReady(){
+  $('#register').on('click', register);
+  $('#addPet').on('click', addPet);
 
-  console.log('jquery sourced');
+  // $(document).on('click', '#update', updatePet);
+  $(document).on('click', '.delete', deletePet);
+  $(document).change('.checkinout', checkInOutPet);
+  getOwners();
+  addTable();
+}
+
+function getOwners(){
+  console.log('in getOwners');
+  $.ajax({
+    url: '/getOwners',
+    type: 'GET',
+    success: function(response){
+      console.log('back from server with:', response);
+      $('#ownerName').empty();
+      for (var i = 0; i < response.length; i++) {
+        $('#ownerName').append('<option id=' + "response[i].ownerfirstname" + "response[i].ownerlastname" + ' data-ownerid='+response[i].id +'>'+ response[i].ownerfirstname + ' ' + response[i].ownerlastname + '</option>');
+      }  //end for loop
+    }  // end success
+  });  //end ajax
+}  //end getOwners
+
+function register(){
+  console.log('in function register');
+  var firstName = $('#firstName').val();
+  var lastName =$('#lastName').val();
+  var nameToRegister = {
+    first: firstName,
+    last: lastName
+  };
+  console.log(nameToRegister);
+  $.ajax ({
+    url: '/register',
+    type: 'POST',
+    data: nameToRegister,
+    success: function (response){
+      console.log('back from server with response',response);
+      $('#ownerName').empty();
+      getOwners();
+    }
+  });
+}
+
+function addPet(){
+  var id = $('#ownerName').find(':selected').data('ownerid');
+  var color = $('#color').val();
+  var breed = $('#breed').val();
+  var petname = $('#petName').val();
+
+  var petToRegister = {
+    id: id,
+    color: color,
+    breed: breed,
+    petName: petname
+  };
+  console.log('sending pet:', petToRegister);
+
+  $.ajax ({
+    url: '/newPet',
+    type: 'POST',
+    data: petToRegister,
+    success: function (response){
+      console.log('back from server with response',response);
+      addTable();
+    }
+  });
+}
+
+function addTable(response) {
+  $.ajax ({
+    url: '/getTable',
+    type: 'GET',
+    success: function(response){
+      console.log('back from server with all table:', response);
+      $('.newrow').remove();
+      for (var i = 0; i < response.length; i++) {
+        $('table').append('<tr class="newrow"></tr>');
+        $('.newrow').last().append('<td>'+response[i].ownerfirstname + " " + response[i].ownerlastname+'</td><td>'+response[i].petname+'</td><td>'+response[i].breed+'</td><td>'+response[i].color+
+        '</td><td><button class="update">Go</button></td><td><button class="delete">Go</button></td><td><select class="checkinout"><option>In</option><option>Out</option></td>');
+      }  //end for loop
+    }  // end success
+  });  //end ajax
+}
+
+function deletePet(){
+  console.log('deleted your pet');
+  var id = $('#ownerName').find(':selected').data('ownerid');
+
+  var deleteToSend = {
+    id: id
+  };
+
+  $.ajax ({
+    url:'/deletePet',
+    type: 'DELETE',
+    data: deleteToSend,
+    success: function(response){
+      console.log('back from server with: ', response);
+      addTable();
+    } //end success
+  });
+}
+
+function checkInOutPet (){
+  console.log('checked pet');
+
+  if ($('.checkinout').val() == 'Out' ){
+    console.log('your pet checked out');
+  }
+
+  else {
+    console.log('your pet checked in');
+  }
 }
--- server.js
+++ server.js
@@ -12,9 +12,162 @@ var pg = require('pg');
 app.use(express.static('public') );
 app.use(bodyParser.urlencoded( { extended: true} ) );
 
+//set up config for pool
+var config = {
+  database: 'pet_hotel',
+  host: 'localhost',
+  port: 5432,
+  max: 20
+};
 
+//create new pool
+var pool = new pg.Pool(config);
 
 //spin up server
 app.listen( 3000, function(){
   console.log("server up on 3000");
+});
+
+//ROUTES
+app.get ('/getOwners', function (req, res){
+  console.log('hit get owners');
+  // array of owners
+  var allOwners = [];
+  // connect to db
+  pool.connect( function( err, connection, done ){
+    //check if there was an Error
+    if( err ){
+      console.log( err );
+      // respond with PROBLEM!
+      res.send( 400 );
+    }// end Error
+    else{
+      console.log('connected to db');
+      // send query for all owners in the 'pets' table and hold in a variable (resultSet)
+      var resultSet = connection.query( "SELECT * from pets" );
+      // convert each row into an object in the allOwners array
+      // on each row, push the row into allOwners
+      resultSet.on( 'row', function( row ){
+        allOwners.push( row );
+      }); //end on row
+      // on end of rows send array as response
+      resultSet.on( 'end', function(){
+        // close connection to reopen spot in pool
+        done();
+        // res.send array of owners
+        res.send( allOwners );
+      }); //end on end
+    } // end no error
+  }); //end pool
+}); //end owners get
+
+
+// Begin Register Post
+app.post('/register', function(req, res){
+
+  console.log('hit register route');
+  var newOwner = req.body;
+  console.log('received from client:', newOwner);
+
+
+  pool.connect( function( err, connection, done ){
+    //check if there was an Error
+    if( err ){
+      console.log( err );
+      // respond with PROBLEM!
+      res.send( 400 );
+    }// end Error
+    else{
+      console.log('connected to db');
+      //query write this owner to db (req.body)
+      connection.query( "INSERT INTO pets (ownerfirstname, ownerlastname) VALUES($1, $2)", [req.body.first, req.body.last]);
+      // close connection to reopen spot in pool
+      done();
+      // res.send
+      res.sendStatus(200);
+    } // end else
+  }); //end pool
+}); //end register post
+
+// NEW PET ROUTE
+app.post('/newPet', function(req, res){
+
+  console.log('hit new pet route');
+  var newPet = req.body;
+  console.log('received from client:', newPet);
+
+
+  pool.connect( function( err, connection, done ){
+    //check if there was an Error
+    if( err ){
+      console.log( err );
+      // respond with PROBLEM!
+      res.send( 400 );
+    }// end Error
+    else{
+      console.log('connected to db');
+      //query write this owner to db (req.body)
+      connection.query( "UPDATE  pets SET petname ='"+ req.body.petName +" 'WHERE id =" + req.body.id);
+      connection.query( "UPDATE  pets SET breed ='"+ req.body.breed +" 'WHERE id =" + req.body.id);
+      connection.query( "UPDATE  pets SET color ='"+ req.body.color +" 'WHERE id =" + req.body.id);
+
+
+      // close connection to reopen spot in pool
+      done();
+      // res.send
+      res.sendStatus(200);
+    } // end else
+  }); //end pool
+}); //end newPet post
+
+//get table route
+app.get ('/getTable', function (req, res){
+  console.log('hit get Table');
+  // array of everything in table
+  var allTable = [];
+  // connect to db
+  pool.connect( function( err, connection, done ){
+    //check if there was an Error
+    if( err ){
+      console.log( err );
+      // respond with PROBLEM!
+      res.send( 400 );
+    }// end Error
+    else{
+      console.log('connected to db');
+      // send query for all owners in the 'pets' table and hold in a variable (resultSet)
+      var resultSet = connection.query( "SELECT * from pets" );
+      // convert each row into an object in the allTable array
+      // on each row, push the row into allTable
+      resultSet.on( 'row', function( row ){
+        allTable.push( row );
+      }); //end on row
+      // on end of rows send array as response
+      resultSet.on( 'end', function(){
+        // close connection to reopen spot in pool
+        done();
+        // res.send array of owners
+        res.send( allTable );
+      }); //end on end
+    } // end no error
+  }); //end pool
+}); //end table get
+
+app.delete ('/deletePet', function (req, res){
+  pool.connect( function( err, connection, done ){
+    //check if there was an Error
+    if( err ){
+      console.log( err );
+      // respond with PROBLEM!
+      res.send( 400 );
+    }// end Error
+    else{
+      console.log('connected to db');
+      // send query for all owners in the 'pets' table and hold in a variable (resultSet)
+      connection.query( "DELETE from pets where id = " + req.body.id );
+      res.sendStatus(200);
+
+      done ();
+    } // end no error
+  }); //end pool
 });
